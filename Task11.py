3c1d86cf9f2b4c814b555c627d6be0977d49831e
unknown <kovgankosvetlana.com>
2022-01-28 21:48:19 +0300

    Task11

--- Task11.py
+++ Task11.py
@@ -1,30 +1,46 @@
 #11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
-result1 = 0
-result2 = 0 
+import random
 def first_digit(n):
     result1 = 0
-    if n < 10 or n > 99:
-        return 'введите другое число'
-    else:
-       result1 = n % 100 / 10
-       result1= int(result1)
+    result1 = n // 10
     return result1
 def second_digit(n):
     result2 = 0
-    if n < 10 or n > 99:
-        return ''
-    else:
-       result2 = n % 10
+    result2 = n % 10
     return result2
 def maximum(n):
     max = first_digit(n)
     if first_digit(n) < second_digit(n):
         max = second_digit(n)
     return max
-n = int(input('Введите число: '))
-print(first_digit(n))
-print(second_digit(n))
+n = random.randint(10,99)
+print('Случаное число из отрезка: ')
+print(n)
+first_digit(n)
+second_digit(n)
 print('Максимальная цифра:')
-print(maximum(n))
+print(maximum(n)) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
