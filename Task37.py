f479a8f729b66d5878d9df0ae5f957bbd62bc578
unknown <kovgankosvetlana.com>
2022-02-02 16:14:18 +0300

    task37

--- Task37.py
+++ Task37.py
@@ -1,20 +1,19 @@
 # 37.Задать список из N элементов, заполненных числами из [-N, N]. Найти произведение элементов на указанных позициях. 
 # Позиции хранятся в файле file.txt в одной строке одно число
 
-data = []
-with open("File37.txt") as f:
-    for line in f:
-        data.append([int(x) for x in line.split()])
-pos1 = data[0]
-pos2 = data[1]
-print(pos1, pos2)
-def elements(lst):
-    N = 5
-    for i in range(-N,N+1):
-        lst.append(i)
-    return lst 
-def multu(lst):
-    return pos2 == lst [10]
-lst = []
-print(elements(lst))
-print(multu(lst))
+from fileinput import close
+from random import randint
+
+N = int(input('Введите число: '))
+lst = [randint(-N,N+1) for i in range(15)]
+print(lst)
+with open('File37.txt', 'r') as F:
+    pos = [int(x) for x in F]
+F.close()
+print(pos)
+result = 1
+for i in range(0,len(lst)):
+    for j in range(0, len(pos)):
+        if pos[j] == i:
+            result *= lst[i]
+print(result)
