1377c6fd0d170db819fa139a5f57b24f2dc42147
unknown <kovgankosvetlana.com>
2022-02-02 17:52:35 +0300

    task40

--- Task40.py
+++ Task40.py
@@ -1,6 +1,4 @@
 # 40.Определить, присутствует ли в заданном списке строк, некоторое число 
 text = ['len', '24', 'apple', 'work', '36']
-def certain_number(n):
-    lst = str(n)
-    return lst in text
-print(certain_number(24))
+text = list(filter(lambda e:'36' in e , text))
+print(text)
