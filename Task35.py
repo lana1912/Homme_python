39aeba8f584d778a2f6ba4d354b9be6e24eeb7c1
unknown <kovgankosvetlana.com>
2022-02-05 11:39:10 +0300

    Task35

--- Task35.py
+++ Task35.py
@@ -1,10 +1,12 @@
-#Написать программу получающую набор произведений чисел от 1 до N.
+# Написать программу получающую набор произведений чисел от 1 до N.
 def set_multiplication(n):
     result = 1
     lst = []
-    for i in range(1,n+1):
+    for i in range(1, n+1):
         result *= i
         lst.append(result)
     return lst
+
+
 n = int(input('Введите число: '))
 print(set_multiplication(n))
