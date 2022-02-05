eefdcc089a80a41b1a8dbc185bc113dbc038e66a
unknown <kovgankosvetlana.com>
2022-02-05 12:54:28 +0300

    Task47

--- Task47.py
+++ Task47.py
@@ -1,13 +1,7 @@
 # 47.Строка содержит набор чисел. Показать большее и меньшее число
-""" A = input('Введите числа: ').split
-for i in range(len(A)): A[i] = int(A[i])
-print(A)
-print(f'Большее число', max(A))
-print(f'Наименьшее число', min(A)) """
 
-A = input('Введите числа  ').split() 
-#for i in range(len(A)): A[i] = int(A[i]) 
+A = input('Введите числа  ').split()
 A = [int(i) for i in A]
-print('Полученный список', A) 
-print(f'Большее число', max(A)) 
-print(f'Меньшее число',min(A))
+print('Полученный список', A)
+print(f'Большее число', max(A))
+print(f'Меньшее число', min(A))
